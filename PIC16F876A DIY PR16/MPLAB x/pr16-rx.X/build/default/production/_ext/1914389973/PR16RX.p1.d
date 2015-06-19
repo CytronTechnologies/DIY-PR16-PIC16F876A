bbbbad970a028e05ebc1b461cb2a876c65d34707
syuhada210 <syuhada@cytron.com.my>
2015-06-19 14:00:54 +0800

    remove

--- PIC16F876A DIY PR16/MPLAB x/pr16-rx.X/build/default/production/_ext/1914389973/PR16RX.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/_ext/1914389973/PR16RX.d  \
- build/default/production/_ext/1914389973/PR16RX.p1:  \
- ../../Source\ file/RF_RX\ MPLABX/PR16RX.c 
--- PIC16F876A DIY PR16/MPLAB x/pr16-tx.X/build/default/production/_ext/1234331603/PR16TX.p1.d
+++
@@ -1,1 +0,0 @@
-build/default/production/_ext/1234331603/PR16TX.p1:	\
