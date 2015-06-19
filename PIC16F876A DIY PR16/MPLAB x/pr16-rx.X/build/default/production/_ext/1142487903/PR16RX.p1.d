ef2967a6026504e66f3b1be927fbf9960a404234
syuhada210 <syuhada@cytron.com.my>
2015-06-19 14:12:44 +0800

    Source code for DIY PR16 Sending data using RF Module
    This is the project folder of sample source code using MPLAB IDE and
    MPLABX IDE either with HITECH C Compiler(V9.80, V9.83) or XC8 for DIY
    project PR16, using PIC16F876A.

---
+++ PIC16F876A DIY PR16/MPLAB x/pr16-rx.X/build/default/production/_ext/1142487903/PR16RX.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/_ext/1142487903/PR16RX.d  \
+ build/default/production/_ext/1142487903/PR16RX.p1:  \
+ ../../Source\ file/PR16RX.c 
---
+++ PIC16F876A DIY PR16/MPLAB x/pr16-tx.X/build/default/production/_ext/1142487903/PR16TX.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/_ext/1142487903/PR16TX.d  \
+ build/default/production/_ext/1142487903/PR16TX.p1:  \
+ ../../Source\ file/PR16TX.c 
